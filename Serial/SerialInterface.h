8bd253e0ed7ce5f30732b4094bae19a5419d4433
Roey Borsteinas <roeyb1@hotmail.com>
2019-10-10 21:07:26 -0400

    Arduino serial interface library

---
+++ Serial/SerialInterface.h
@@ -0,0 +1,29 @@
+#pragma once
+/**
+ * A serial protocol inspired by the PPP specifications.
+ * 1. The flag "[" (0x5B) signifies the start of a message and "]" (0x5D), the end
+ * 2. The flag 0x7E is an escape byte
+ * 3. Each sent message expects an ACK
+ * @author Roey Borsteinas
+ * @date October 7, 2019
+ */
+
+#include <Arduino.h>
+
+class SerialInterface
+{
+public:
+    SerialInterface() {}
+
+    void initialize(int _baudRate);
+
+    void send(const String message);
+    String receive();
+
+private:
+    // These methods are low level and deal directly with binary data sent over serial
+    String read();
+    void write(const String message);
+
+    int baudRate;
+};
