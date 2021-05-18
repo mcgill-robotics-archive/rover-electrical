42324c8bb26095a169a5a1ee2f76d178d7a24230
Sam Dayo <56176274+samdayo2019@users.noreply.github.com>
2021-05-17 22:32:02 -0400

    Updated I2C config, Reader, and Writer

--- I2C/I2C_Config/main.c
+++ I2C/I2C_Config/main.c
@@ -12,6 +12,8 @@
 #include "driverlib/pin_map.h"
 
 void intI2C_CONFIG(uint32_t ui32Peripheral, uint32_t ui32Base, uint8_t ui8SlaveAddr, bool bFast, uint32_t ui32Config);
+void write(uint8_t ui8SlaveAddr, uint32_t ui32Base, bool bReceive, uint8_t num_args);
+uint32_t read(uint8_t ui8SlaveAddr, uint8_t ui8reg, uint32_t ui32Base, bool bReceive); 
 
 int main(void)
 {
@@ -59,6 +61,90 @@ void intI2C_CONFIG(uint32_t ui32Peripheral, uint32_t ui32PeripheralGPIO, uint32_
         //commented out because I don't think our MCU has a FIFO for i2c
         //I2CRxFIFOConfigSet(ui32Base, ui32Config);
 
+}
+
+//Sends an I2C command to the specified slave 
+//This function can be written with a variable agrument list
+//Can access various registers in the slave device and write a value to that register by adding variables to the function's variable list
+void write(uint8_t ui8SlaveAddr, uint32_t ui32Base, bool bReceive, uint8_t num_args, ...){
+    I2CMasterSlaveAddrSet(ui32Base, ui8SlaveAddr, bReceive);
+    
+    //Stores list of data arguments
+    va_list vargs; 
+    
+    //open va list 
+    va_start(vargs, num_args);
+    
+    //Put Data to be sent into the master data register
+    I2CMasterDataPut(ui32Base, va_arg(vargs, uint32_t)); 
+    
+    if(num_args == 1){
+        //Initiate sending data from the MCU
+        I2CMasterControl(ui32Base, I2C_MASTER_CMD_SINGLE_SEND);
+        
+        //Wait until MCU finished transferring data
+        while(I2CMasterBusy(ui32Base));
+        
+        //Close va list
+        va_end(vargs);
+    }
+    
+    //to write to multiple registers on the slave device
+    else{
+        //initialize data transmission from MCU
+        I2CMasterControl(ui32Base, I2C_MASTER_CMD_BURST_SEND_START);
+        
+        //wait until MCU finished transferring data
+        while(I2CMasterBusy(ui32Base));
+        
+        for(uint8_t i = 1; i < (num_args - 1); i++){
+            //load data into MCU register
+            I2CMasterDataPut(ui32Base, va_args(vargs, uint32_t));
+            
+            //Send data that was just loaded into register
+            I2CMasterControL(ui32Base, I2C_MASTER_CMD_BURST_SEND_CONT);
+            
+            //Wait until MCU is done transferring
+            while(I2CMasterBusy(ui32Base));
+        }
+        //load last piece of data into I2C FIFO
+        I2CMasterDataPut(ui32Base, va_args(vargs, uint32_t)); 
+        
+        //send last piece of data from MCU
+        I2CMasterControl(ui32Base, I2C_MASTER_CMD_BURST_SEND_FINISH);
+        
+        //wait until MCU is done transferring
+        while(I2CMasterBusy(ui32Base)); 
+        
+        //close va list
+        va_end(vargs);
+    }
+}
+
+//read from specified register on the slave device
+uint32_t read(uint8_t ui8SlaveAddr, uint8_t ui8reg, uint32_t ui32Base, bool bReceive){
+    //Write register address to slave 
+    I2CMasterSlaveAddrSet(ui32Base, ui8SlaveAddr, false);
+    
+    //Specify register we want to read from
+    I2CMasterDataPut(ui32Base, ui8reg); 
+    
+    //Send control byte and register address byte to slave 
+    I2CMasterControl(ui32Base, I2C_MASTER_CMD_BURST_SEND_START);
+    
+    //Wait until MCU done transferring
+    while(I2CMasterBusy(ui32base)); 
+    
+    //Tell slave we will be reading data from it
+    I2CMasterSlaveAddrSet(ui32Base, ui8SlaveAddr, bReceive); 
+    
+    I2CMasterControl(ui32Base, I2C_MASTER_CMD_SINGLE_RECEIVE); 
+    
+    //Wait until MCU is done transferring 
+    while(I2CMasterBusy(ui32Base)); 
+    
+    //return the data obtained from the register
+    return I2CMasterDataGet(ui32Base); 
 }
 
 
